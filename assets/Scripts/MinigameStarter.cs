4d051d52b27e4b817fb7375be744dcab9fd6420a
Robert Schendzielorz <r.t.s@web.de>
2016-05-26 10:57:43 +0200

    RocketScene( called CandyScene ) And Level Hub

---
+++ assets/Scripts/MinigameStarter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinigameStarter : MonoBehaviour {
+	[SerializeField] bool _active;
+	[SerializeField] private string _minigameScene;
+
+	private Color _defaultColor;
+
+	private void Start()
+	{
+		_defaultColor = GetComponent<Renderer>().sharedMaterial.GetColor("_Color");
+	}
+
+	public void Update()
+	{
+		if (_active)
+		{
+			GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+		}
+		else
+		{
+			GetComponent<Renderer>().material.SetColor("_Color", _defaultColor);
+		}
+	}
+
+	public void StartMiniGame()
+	{
+		if (_active)
+		{
+			FindObjectOfType<GameManager>().StartMinigame(_minigameScene);
+			_active = false;
+		}
+	}
+
+	public bool Startable
+	{
+		set { _active = value; }
+	}
+
+	public bool Active
+	{
+		get { return _active; }
+	}
+}
---
+++ assets/Scripts/MinigameTransititon.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinigameTransititon : MonoBehaviour {
+	[SerializeField] private float _changePerSecond = 0.5f;
+	[SerializeField] private float _heightWidthEndValue = 0.9f;
+	[SerializeField] private float _xyEndValue = 0.05f;
+
+	private Camera _camera;
+
+	private void Start()
+	{
+		_camera = GetComponent<Camera>();
+
+		_camera.rect = new Rect(0.5f, 0.5f, 0.0f, 0.0f);
+	}
+
+	private void Update()
+	{
+		Rect viewPort = _camera.rect;
+
+		viewPort.width += _changePerSecond * Time.deltaTime;
+		viewPort.height += _changePerSecond * Time.deltaTime;
+
+		if (viewPort.height >= _heightWidthEndValue || viewPort.width >= _heightWidthEndValue)
+		{
+			viewPort.width = _heightWidthEndValue;
+			viewPort.height = _heightWidthEndValue;
+		}
+
+		viewPort.x -= _changePerSecond / 2.0f * Time.deltaTime;
+		viewPort.y -= _changePerSecond / 2.0f * Time.deltaTime;
+
+		if (viewPort.x <= _xyEndValue || viewPort.y <= _xyEndValue)
+		{
+			viewPort.x = _xyEndValue;
+			viewPort.y = _xyEndValue;
+		}
+
+		_camera.rect = viewPort;
+	}
+}
