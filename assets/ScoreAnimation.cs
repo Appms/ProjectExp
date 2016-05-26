7d25f9ea05ad7e91d6dc50ba056653c6721a5f74
AMS <amserik@hotmail.com>
2016-05-27 00:52:39 +0200

    Updated HUD

---
+++ assets/ScoreAnimation.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private Color _normalColor;
+
+    [SerializeField]
+    private Color _animationColor;
+
+    [SerializeField]
+    private AnimationCurve _animationCurve;
+
+    [SerializeField]
+    private float _animationSpeed;
+
+    private float _animationDelta;
+
+    private int _scoreGame;
+    private int _score;
+
+    private int _originalFontSize;
+
+    private Text _scoreLabel;
+
+    private float _acc;
+
+    private AudioSource _audio;
+
+    // Use this for initialization
+    void Start () {
+        _scoreLabel = GetComponent<Text>();
+        _originalFontSize = _scoreLabel.fontSize;
+
+        _audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(_scoreGame != _score && _animationDelta < 1)
+        {
+            _acc += Time.unscaledDeltaTime * 2;
+            _animationDelta += Time.unscaledDeltaTime * (_animationSpeed + _acc);
+            _scoreLabel.color = Color.Lerp(_normalColor, _animationColor, 1 - _animationCurve.Evaluate(_animationDelta));
+            _scoreLabel.fontSize = (int)(_originalFontSize * _animationCurve.Evaluate(_animationDelta));
+        }
+
+        else if (_animationDelta >= 1)
+        {
+            _score++;
+            _audio.Play(0);
+            _animationDelta = 0;
+            _scoreLabel.text = _score.ToString();
+        }
+
+        else if (_scoreGame == _score)
+        {
+            _acc = 0;
+        }
+    }
+
+    public void UpdateScore(int newScore)
+    {
+        _scoreGame = newScore;
+    }
+}
--- assets/Scripts/MaingameManager.cs
+++ assets/Scripts/MaingameManager.cs
@@ -13,6 +13,10 @@ public class MaingameManager : MonoBehaviour
     private GameObject _hud;
 
     [SerializeField]
+    [Tooltip("UI text that displays the time.")]
+    private Text _gameTimer;
+
+    [SerializeField]
 	[Tooltip("Determines the time until a help message is displayed.")]
 	private float _timeToHelpMessage;
 
@@ -50,6 +54,10 @@ public class MaingameManager : MonoBehaviour
 
 	//Safety bool to only upload score once
 	private bool _uploadedScore = false;
+
+    //Timer for the game
+    //TODO Read it from the server
+    private float _timer = 180;
 
 
     /// <summary>
@@ -102,8 +110,12 @@ public class MaingameManager : MonoBehaviour
 	
 	private void Update()
 	{
-		//TODO Replace with touch Input
-		if (Input.anyKey /*|| Input.GetTouch()*/)
+        _timer -= Time.unscaledDeltaTime;
+        if (Mathf.FloorToInt(_timer % 60) < 10) _gameTimer.text = Mathf.FloorToInt(_timer / 60) + ":0" + Mathf.FloorToInt(_timer % 60);
+        else _gameTimer.text = Mathf.FloorToInt(_timer / 60) + ":" + Mathf.FloorToInt(_timer % 60);
+
+        //TODO Replace with touch Input
+        if (Input.anyKey /*|| Input.GetTouch()*/)
 		{
 			_lastInputTime = Time.time;
 		}
@@ -178,6 +190,7 @@ public class MaingameManager : MonoBehaviour
         _score += pScore;
 
         _minigames[Mathf.Clamp(++_minigameUnlock, 0, _minigames.Length)].interactable = true;
+        FindObjectOfType<ScoreAnimation>().UpdateScore(_score);
     }
 
 	/// <summary>
