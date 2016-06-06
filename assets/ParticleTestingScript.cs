05e352bdb1f03877ada50a1a01b5719ad5ceb45e
Robert Schendzielorz <r.t.s@web.de>
2016-06-06 16:00:56 +0200

    Particle Move and Particle Manager
    Moved particles to Resources folder and included ParticleManager

---
+++ assets/ParticleTestingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleTestingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.K)){
+			ParticleManager.CreateParticle("Particles/GetPointsParticle", 2f, new Vector3(0,0,0));
+		}
+		if(Input.GetKeyDown(KeyCode.J)){
+			ParticleManager.CreateParticle(ParticleManager.Particles.PosFeedback, 2f, new Vector3(0,0,0));
+		}
+	}
+}
---
+++ assets/Personal folders/Robert/Scripts/ParticleManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleManager : MonoBehaviour {
+
+	public enum Particles {GetPoints, PosFeedback}
+
+
+	Dictionary<Particles, string> particleDic = new Dictionary<Particles, string>();
+
+	void Start(){
+		particleDic.Add(Particles.GetPoints, "Particles/GetPointsParticle");
+		particleDic.Add(Particles.PosFeedback, "Particles/GreenSparkleParticle");
+	}
+
+
+	static public void CreateParticle(string path, float duration, Vector3 position){
+		GameObject go = (GameObject)Instantiate(Resources.Load(path), position, Quaternion.identity);
+		Destroy(go, duration);
+	}
+
+	static public void CreateParticle(Particles particle, float duration, Vector3 position){
+		GameObject go = (GameObject)Instantiate(Resources.Load(particleDic[particle]), position, Quaternion.identity);
+		Destroy(go, duration);
+	}
+}
