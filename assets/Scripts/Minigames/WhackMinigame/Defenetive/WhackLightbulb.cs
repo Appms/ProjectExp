6877d058a536fcb627448213f3b1d365d2b250c7
Daniel H <danielhenseler@badcodes.de>
2016-06-07 23:35:47 +0200

    Implemented Whack minigame scene

---
+++ assets/Scripts/Minigames/WhackMinigame/Defenetive/WhackLightbulb.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WhackLightbulb : WhackObject
+{
+	[SerializeField]
+	private Material _offMaterial;
+
+	[SerializeField]
+	private Material _onMaterial;
+
+	protected override void TurnOn()
+	{
+		GetComponent<Light>().enabled = true;
+		GetComponent<Animator>().SetTrigger("Switch");
+		GetComponent<Renderer>().material = _onMaterial;
+	}
+
+	protected override void TurnOff()
+	{
+		GetComponent<Light>().enabled = false;
+		GetComponent<Animator>().SetTrigger("Switch");
+		GetComponent<Renderer>().material = _offMaterial;
+	}
+
+	protected override void InteractRight()
+	{
+		GetComponent<Light>().enabled = false;
+		GetComponent<Animator>().SetTrigger("Right");
+		GetComponent<Renderer>().material = _offMaterial;
+	}
+
+	protected override void InteractWrong()
+	{
+		GetComponent<Animator>().SetTrigger("Wrong");
+	}
+}
