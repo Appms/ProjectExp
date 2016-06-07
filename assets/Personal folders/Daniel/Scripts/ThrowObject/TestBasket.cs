4158673315a6f0827b250e32bd36cfa8c8706459
Robert Schendzielorz <r.t.s@web.de>
2016-06-07 21:28:23 +0200

    Playable Sorting and Throwing game. Particle Fix
    - fixed a bug where the PosFeedback particle was not displayed in
    minigames.

--- assets/Personal folders/Daniel/Scripts/ThrowObject/TestBasket.cs
+++ assets/Personal folders/Daniel/Scripts/ThrowObject/TestBasket.cs
@@ -2,10 +2,34 @@
 using System.Collections;
 
 public class TestBasket : T_SpawnObject
-{
+{	
+	[SerializeField]
+	Vector3 Pos1;
+	[SerializeField]
+	Vector3 Pos2;
+
+	private Vector3 _originPos;
+
+	protected override void Start(){
+		base.Start();
+		_originPos = this.transform.position;
+		ParticleManager.InitParticles();
+	}
+
 	protected override void Spawn()
 	{
 		base.Spawn();
 		GetComponent<Animator>().SetTrigger("SpawnObject");
+	}
+
+	protected override void OnRelease(){
+		base.OnRelease();
+		ResetSpawn();
+	}
+	private void ResetSpawn(){
+		Debug.Log("ResetSpawn");
+		this.transform.position = new Vector3(	_originPos.x + Random.Range(Pos1.x, Pos2.x), 
+												_originPos.y + Random.Range(Pos1.y, Pos2.y), 
+												_originPos.z + Random.Range(Pos1.z, Pos2.z));
 	}
 }
