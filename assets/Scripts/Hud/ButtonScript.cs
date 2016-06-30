fa4a1a87b41870d09faaa5b58f35bfed0a735072
Daniel H <danielhenseler@badcodes.de>
2016-06-30 13:54:48 +0200

    Made some modifiations for playtest

--- assets/Scripts/Hud/ButtonScript.cs
+++ assets/Scripts/Hud/ButtonScript.cs
@@ -15,51 +15,39 @@ public class ButtonScript : MonoBehaviour {
 	GameObject[] buttons;
 	GameObject[] stars;
 
-    [SerializeField]
-    private GameObject _hud;
+	[SerializeField]
+	private GameObject _hud;
 
 	// Use this for initialization
 	void Start () {
 
-		buttons = GameObject.FindGameObjectsWithTag ("Button");
-		stars = GameObject.FindGameObjectsWithTag ("Star");
+		buttons = GameObject.FindGameObjectsWithTag("Button");
+		stars = GameObject.FindGameObjectsWithTag("Star");
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-        if (_hud.activeInHierarchy)
-        {
-            foreach (GameObject button in buttons)
-            {
-                if (button.GetComponent<Button>().interactable)
-                {
-					if (button.GetComponent<StarScript>().Played)
-					{
+		if (_hud.activeInHierarchy) {
+			foreach (GameObject button in buttons) {
+				if (button.GetComponent<Button>().interactable) {
+					if (button.GetComponent<StarScript>().Played) {
 						button.GetComponentInChildren<Image>().sprite = buttonPics[2];
-					}
-					else
-					{
+					} else {
 						button.GetComponentInChildren<Image>().sprite = buttonPics[0];
 					}
-                }
-                else
-                {
-                    button.GetComponentInChildren<Image>().sprite = buttonPics[1];
-                }
-            }
+				} else {
+					button.GetComponentInChildren<Image>().sprite = buttonPics[1];
+				}
+			}
 
-            foreach (GameObject star in stars)
-            {
-                if (star.GetComponentInParent<Button>().interactable)
-                {
-                    star.GetComponentInChildren<Image>().sprite = starPics[0];
-                }
-                else
-                {
-                    star.GetComponentInChildren<Image>().sprite = starPics[1];
-                }
-            }
-        }
+			foreach (GameObject star in stars) {
+				if (star.GetComponentInParent<Button>().interactable) {
+					star.GetComponentInChildren<Image>().sprite = starPics[0];
+				} else {
+					star.GetComponentInChildren<Image>().sprite = starPics[1];
+				}
+			}
+		}
 	}
 }
